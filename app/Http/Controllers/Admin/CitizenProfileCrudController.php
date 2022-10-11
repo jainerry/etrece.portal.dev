ee6170d7c6297ef90b82e86c373f1ba24a920c42
edgejohn <johncarlosacrosalazar@gmail.com>
2022-10-11 23:26:47 +0800

    update

---
+++ app/Http/Controllers/Admin/CitizenProfileCrudController.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\CitizenProfileRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Yajra\Address\HasAddress;
+
+/**
+ * Class CitizenProfileCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class CitizenProfileCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\CitizenProfile::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/citizen-profile');
+        CRUD::setEntityNameStrings('citizen profile', 'citizen profiles');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::column('address');
+        CRUD::column('bdate');
+        CRUD::column('brgyID');
+        CRUD::column('civilStatus');
+        CRUD::column('created_at');
+        CRUD::column('fName');
+        CRUD::column('isActive');
+        CRUD::column('lName');
+        CRUD::column('mName');
+        CRUD::column('name');
+        CRUD::column('placeOfOrigin');
+        CRUD::column('purokID');
+        CRUD::column('refID');
+        CRUD::column('sex');
+        CRUD::column('suffix');
+        CRUD::column('updated_at');
+
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - CRUD::column('price')->type('number');
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(CitizenProfileRequest::class);
+
+        $this->crud->addField([
+            'name'=>'fName',
+            'label'=>'First Name',
+            'allows_null' => false,
+            'wrapperAttributes' => [
+                'class' => 'form-group col-12 col-lg-4'
+           ]
+        ]);
+        $this->crud->addField([
+            'name'=>'mName',
+            'label'=>'Middle Name',
+            'allows_null' => true,
+            'hint'=>'optional',
+            'wrapperAttributes' => [
+                'class' => 'form-group col-12 col-lg-4'
+           ]
+        ]);
+        $this->crud->addField([
+            'name'=>'lName',
+            'label'=>'Last Name',
+            'allows_null' => false,
+            'wrapperAttributes' => [
+                'class' => 'form-group col-12 col-lg-4'
+           ]
+           
+        ]);
+        $this->crud->addField([
+            'name'=>'address',
+            'label'=>'Address',
+            'allows_null' => false,
+            'wrapperAttributes' => [
+                'class' => 'form-group col-12 col-lg-6'
+           ]
+           
+        ]);
+        $this->crud->addField([
+            'name'=>'bdate',
+            'label'=>'Birthday',
+            'allows_null' => false,
+            'wrapperAttributes' => [
+                'class' => 'form-group col-12 col-lg-6'
+           ]
+           
+        ]);
+        $this->crud->addField([
+            'name'=>'brgyID',
+            'label'=>'Baranggay',
+            'allows_null' => false,
+            'wrapperAttributes' => [
+                'class' => 'form-group col-12 col-lg-6'
+           ]
+        ]);
+       
+        $this->crud->addField([   // select_from_array
+            'name'        => 'civilStatus',
+            'label'       => "Civil Status",
+            'type'        => 'select_from_array',
+            'options'     => ['Single' => 'Single', 
+                              'Married' => 'Married',
+                              'Widowed' => 'Widowed'],
+            'allows_null' => false,
+            'default'     => '1',
+            'wrapperAttributes' => [
+                'class' => 'form-group col-12 col-lg-6'
+            ]
+            // 'allows_multiple' => true, // OPTIONAL; needs you to cast this to array in your model;
+        ]);
+        
+        CRUD::field('isActive');
+        // CRUD::field('placeOfOrigin');
+       $this->crud->addField([   // Textarea
+            'name'  => 'placeOfOrigin',
+            'label' => 'Place of Origin',
+            'type'  => 'textarea'
+        ]);
+        CRUD::field('purokID');
+        CRUD::field('refID');
+        $this->crud->addField([   // select_from_array
+            'name'        => 'sex',
+            'label'       => "Sex",
+            'type'        => 'select_from_array',
+            'options'     => ['1' => 'Male', '0' => 'Female'],
+            'allows_null' => false,
+            'default'     => '1',
+            // 'allows_multiple' => true, // OPTIONAL; needs you to cast this to array in your model;
+        ]);
+        CRUD::field('suffix');
+
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
