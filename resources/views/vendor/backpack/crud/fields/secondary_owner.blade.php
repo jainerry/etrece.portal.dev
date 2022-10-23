717c301cca70e450c7cabd2794a6d19c6bf5b42c
edgejohn <johncarlosacrosalazar@gmail.com>
2022-10-23 16:24:57 +0800

    Update secondary_owner.blade.php

--- resources/views/vendor/backpack/crud/fields/secondary_owner.blade.php
+++ resources/views/vendor/backpack/crud/fields/secondary_owner.blade.php
@@ -1,31 +1,41 @@
 {{-- secondary_owner_field field --}}
 @php
-    $connected_entity = new $field['model']();
+    $connected_entity = new $field['model'];
     $connected_entity_key_name = $connected_entity->getKeyName();
     $field['value'] = old_empty_or_null($field['name'], '') ?? ($field['value'] ?? ($field['default'] ?? ''));
     $field['delay'] = $field['delay'] ?? 500;
-    $field['minimum_input_length'] = $field['minimum_input_length'] ?? 2;
     $field['attribute'] = $field['attribute'] ?? $connected_entity->identifiableAttribute();
-    $old_value = old_empty_or_null($field['name'], false) ?? ($field['value'] ?? ($field['default'] ?? false));
+    $old_value = old_empty_or_null($field['name'], false) ??  $field['value'] ?? $field['default'] ?? false;
 @endphp
 
 @include('crud::fields.inc.wrapper_start')
 <label>{!! $field['label'] !!}</label>
 @include('crud::fields.inc.translatable_icon')
+<input type="hidden" name="{{ $field['name'] }}" value="" @if(in_array('disabled', $field['attributes'] ?? [])) disabled @endif />
+<select name="{{ $field['name'] }}[]" 
+    id="" style="width: 100%" 
+    data-init-function="bpFieldInitDummyFieldElement"
+    data-data-source="{{ $field['data_source'] }}" 
+    data-ajax-delay="{{ $field['delay'] }}"
+    data-field-is-inline="{{var_export($inlineCreate ?? false)}}"
+    data-connected-entity-key-name="{{ $connected_entity_key_name }}"
+    data-dependencies="{{ isset($field['dependencies'])?json_encode(Arr::wrap($field['dependencies'])): json_encode([]) }}"
+    @include('crud::fields.inc.attributes', ['default_class' =>  'form-control'])
+    class="js-data-example-ajax col-lg-12" multiple>
 
-<input type="hidden" name="{{ $field['name'] }}" value="" @if (in_array('disabled', $field['attributes'] ?? [])) disabled @endif data-init-function="bpFieldInitDummyFieldElement" />
-
-{{-- v2 of secondary_owner --}}
-<div class="card">
-    <div class="card-body">
-
-        <div>
-            <button class="btn btn-default btn-secondary-owner-add">Add Secondary Owner</button>
-        </div>
-    </div>
-</div>
-
-
+    @if ($old_value)
+    @foreach ($old_value as $item)
+        @if (!is_object($item))
+            @php
+                $item = $connected_entity->find($item);
+            @endphp
+        @endif
+        <option value="{{ $item->getKey() }}" selected>
+            {{ $item->{$field['attribute']} }}
+        </option>
+    @endforeach
+@endif
+</select>
 
 {{-- HINT --}}
 @if (isset($field['hint']))
@@ -40,31 +50,8 @@
 
     @loadOnce('secondary_owner_field_style')
     <style>
-        .search_secondary_container {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: #0000006b;
-            overflow-y:scroll;
-        }
-
-        .search_secondary_container .box {
-            width: 700px;
-            max-width: 100%;
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-            min-height: 350px;
-            background-color: white;
-            left: 0;
-            right: 0;
-            margin: auto;
-            border-radius: 15px;
-            max-height: 500px;
-            overflow-y: auto;
-            
+        .secondary_owner_field_class {
+            display: none;
         }
     </style>
     @endLoadOnce
@@ -81,133 +68,97 @@
     @loadOnce('bpFieldInitDummyFieldElement')
         <script>
             function bpFieldInitDummyFieldElement(element) {
-                let body = $('body')
-
-                function secondary_owner_search_template() {
-
-                    return `<div class="search_secondary_container " style="display:none;">
-                             <div class="box p-3"> 
-                                <div class="container-fluid"> 
-                                    <div class="row">
-                                        <div class="col-6"> 
-                                            <label> Search by Name or Reference No: </label> <br>
-                                            <input class="form-control search-input-secondary" placeholder="Search Text...">
-                                        </div>
-                                        <div class=" col-12 mt-3 search-result"> 
-
-                                        </div>
-                                    </div>
-                                </div>
-                             </div>
-                        </di>`;
-                }
-
-                function searching_template() {
-                    return `<div class="text-center">
-                        Searching...
-                         </div>`;
-                }
-                $(".btn-secondary-owner-add").click(function(e) {
-                    e.preventDefault();
-                    e.stopImmediatePropagation();
-                    let container = $('.search_secondary_container');
-
-                    if (container.length == 0) {
-
-                        $('body').append(secondary_owner_search_template)
-                        $('.search_secondary_container').fadeIn();
-                        $.ah
-                    }
-                })
-                $("body").on('click', '.search_secondary_container', function(e) {
-                    $(this).fadeOut(function(e) {
-                        $(this).remove();
-                    })
-                })
-                $("body").on('click', '.search_secondary_container .box', function(e) {
-                    e.stopImmediatePropagation();
-                })
-                let timeOutInput = null;
-
-                $("body").on('keyup', '.search-input-secondary', function(e) {
-                    let val = $(this).val();
-                    let length = val.length;
-                    let resTemp = [`<ul class="p-0" style="list-style:none">`];
-                    if (val == "") {
-                        $('.search-result').html()
-                        return;
-                    }
-                    clearTimeout(timeOutInput);
-                    if (length >= 3) {
-                        timeOutInput = setTimeout(() => {
-                            $('.search-result').html(searching_template);
-                            $.ajax({
-                                url: '{{ $field['data_source'] }}',
-                                method: 'GET',
-                                data: {
-                                    q: val
-                                },
-                                success: function(e) {
-                                    $.each(e, function() {
-                                        resTemp.push(`<li class="border p-3 mb-2 l" data-json="${JSON.stringify(this)}">
-                                                        <div class="col-lg-9">
-                                                            <div>
-                                                                <input type="checkbox" class="citId pe-1 d-none" value="${this.id}">
-                                                                <strong>Name: ${this.fullname} - ${this.refID} </strong>
-                                                                </div>
-                                                                <div>
-                                                                Barangay: ${this.barangay.name}
-                                                                </div>
-                                                                <div>
-                                                                Birthday: ${this.bdate}
-                                                                </div>
-                                                                <div>
-                                                                Civil Status: ${this.civilStatus} 
-                                                                </div>
-                                                        </div>
-                                                        
-
-                                                    </li>`)
-                                    })
-                                    resTemp.push(`</ul>`)
-                                    $('.search-result').html(resTemp.join(''))
-                                }
-                            })
-                        }, 500);
-
-                    }
-                })
                 // this function will be called on pageload, because it's
                 // present as data-init-function in the HTML above; the
                 // element parameter here will be the jQuery wrapped
                 // element where init function was defined
-
                 var $dataSource = element.attr('data-data-source');
                 var $isFieldInline = element.data('field-is-inline');
                 var $dependencies = JSON.parse(element.attr('data-dependencies'));
-                var $minimumInputLength = element.attr('data-minimum-input-length');
-
-                function formatState(el) {
-                    // console.log($(.attr('data-barangay'))
-                    element = `<div>
-                                                <div class="pb-1"><b>${el.text}</b></div>
+                console.log(element.val());
+                $(element).select2({
+                    theme: 'bootstrap',
+                    multiple: true,
+                    placeholder: 'Select Secondary Owner',
+                    minimumInputLength: 2,
+                    allowClear: true,
+                    dropdownParent: $isFieldInline ? $('#inline-create-dialog .modal-content') : $(document.body),
+                    ajax: {
+                        url: '{{ $field['data_source'] }}',
+                        method: 'GET',
+                        delay: "{{ $field['delay'] }}",
+                        data: function(params) {
+                            return {
+                                q: params.term
+                            }
+                        },
+                        processResults: function(data) {
+                            let paginate = false;
+                            console.log({
+                                results: $.map(data, function() {
+                                    return {
+                                        text: this.entry_data,
+                                        id: this.id
+                                    }
+                                }),
+                                pagination: {
+                                    more: paginate,
+                                }
+                               
+                            })
+                            return {
+                                results: $.map(data, function(item) {
+                                    console.log(item.fName)
+                                    return {
+                                        text: `<div>
+                                                <div class="pb-1"><b>${item.fName} ${item.mName} ${item.lName}</b></div>
                                                 <div> 
                                                     <div>
-                                                        <b>birthdate:</b> <span> ${el.element.dataset.bdate} </span>
+                                                        <b>birthdate:</b> <span> ${item.bdate} </span>
                                                     </div>
                                                     <div>
-                                                        <b>barangay:</b> <span> ${el.element.dataset.barangay} </span>
+                                                        <b>barangay:</b> <span> ${item.barangay.name} </span>
                                                     </div>
                                                 </div>
-                                            </div>`;
-
-                    return $(element);
-                }
-
-
-             
-
-
+                                            </div>`,
+                                        id: item.id
+                                    }
+                                }),
+                                pagination: {
+                                    more: paginate,
+                                }
+                               
+                            }
+                        },
+                        cache: true
+                    },
+                    escapeMarkup: function (markup) { return markup; }, 
+                })
+                  // if any dependencies have been declared
+        // when one of those dependencies changes value
+        // reset the select2 value
+        for (var i=0; i < $dependencies.length; i++) {
+            var $dependency = $dependencies[i];
+            //if element does not have a custom-selector attribute we use the name attribute
+            if(typeof element.attr('data-custom-selector') == 'undefined') {
+                form.find('[name="'+$dependency+'"], [name="'+$dependency+'[]"]').change(function(el) {
+                        $(element.find('option:not([value=""])')).remove();
+                        element.val(null).trigger("change");
+                });
+            }else{
+                // we get the row number and custom selector from where element is called
+                let rowNumber = element.attr('data-row-number');
+                let selector = element.attr('data-custom-selector');
+                // replace in the custom selector string the corresponding row and dependency name to match
+                selector = selector
+                    .replaceAll('%DEPENDENCY%', $dependency)
+                    .replaceAll('%ROW%', rowNumber);
+                $(selector).change(function (el) {
+                    $(element.find('option:not([value=""])')).remove();
+                    element.val(null).trigger("change");
+                });
+            }
+        }
             }
         </script>
     @endLoadOnce
