d8061ac00af25f55e71b591674ef446efa845dac
edgejohn <john@edgedigital.sg>
2022-11-24 01:20:08 +0800

    Update BusinessTypeRequest.php

--- app/Http/Requests/BusinessTypeRequest.php
+++ app/Http/Requests/BusinessTypeRequest.php
@@ -25,7 +25,8 @@ class BusinessTypeRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name' => 'required|unique:App\Models\BusinessType,name,'.$this->id,
+            'description' => 'required'
         ];
     }
 
