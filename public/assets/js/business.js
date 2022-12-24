a687099394d6e0fdcccb4d8d4c0b773f6c4128f4
edgejohn <john@edgedigital.sg>
2022-12-24 15:16:59 +0800

    Update business.js

--- public/assets/js/business.js
+++ public/assets/js/business.js
@@ -5,6 +5,13 @@ $(function(){
       // checkSameHead.prependTo(buss_act_add)
    }
    crud.field("range_box").hide()
+   if(crud.field('type').value == "Range"){
+      crud.field('range_box').show()
+   }else{
+      crud.field('range_box').hide()
+   }
+
+   
    crud.field('type').onChange(function(field){
       if(field.value == "Range"){
          crud.field('range_box').show()
