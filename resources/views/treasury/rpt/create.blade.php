d847e5dde901d1fa5e474e1b406cee3717e5ce1c
jai2038 <90070754+jai2038@users.noreply.github.com>
2022-12-28 01:24:52 +0800

    treasury-print-or

--- resources/views/treasury/rpt/create.blade.php
+++ resources/views/treasury/rpt/create.blade.php
@@ -93,3 +93,22 @@
     </div>
 </div>
 
+<div class="modal fade printModal" role="dialog">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="printModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+              
+            </div>
+            <div class="modal-footer">
+		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-success">Print</button>
+		    </div>
+        </div>
+    </div>
+</div>
